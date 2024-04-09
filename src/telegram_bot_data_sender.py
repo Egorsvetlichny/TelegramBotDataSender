19bf998aab55cf7b23675e86ed20ce95ed34734b
Egor Svetlichny <fkhor@inbox.ru>
2024-04-09 19:56:23 +0300

    change forward function with other information

--- src/telegram_bot_data_sender.py
+++ src/telegram_bot_data_sender.py
@@ -31,37 +31,42 @@ def handle_start(message):
 # Блок функции forward
 @bot.message_handler(commands=['forward'])
 def forward_message(message):
-    bot.send_message(message.chat.id, "Пожалуйста, напишите своё ФИО.")
-    bot.register_next_step_handler(message, send_fio_to_admin)
+    bot.send_message(message.chat.id, "Укажите свою Фамилию и Имя")
+    bot.register_next_step_handler(message, remember_user_fio)
 
     logger.info("Пользователь %s использовал функцию forward.", get_user_full_name(message))
 
 
-def send_fio_to_admin(message):
+def remember_user_fio(message):
     data_arr = [' '.join(message.text.split()[1:]), message.text]
-    bot.send_message(message.chat.id, "Укажите свою дату рождения через точку.")
-    bot.register_next_step_handler(message, send_birthdate_to_admin, data_arr)
+    bot.send_message(message.chat.id, "Укажите дату рождения в формате: дд.мм.гггг")
+    bot.register_next_step_handler(message, remember_user_birthdate, data_arr)
 
     logger.info("Пользователь %s указал своё ФИО", get_user_full_name(message))
 
 
-def send_birthdate_to_admin(message, data_arr):
-    # bot.forward_message(admin_chat_id, message.chat.id, message.message_id)
+def remember_user_birthdate(message, data_arr):
     data_arr.append(message.text)
-    bot.send_message(message.chat.id, "Последним шагом, укажите серию и номер вашего паспорта.")
-    bot.register_next_step_handler(message, send_passport_data_to_admin, data_arr)
+    bot.send_message(message.chat.id, "Укажите контактный номер телефона")
+    bot.register_next_step_handler(message, remember_user_phone_number, data_arr)
 
     logger.info("Пользователь %s указал свою дату рождения", get_user_full_name(message))
 
 
-def send_passport_data_to_admin(message, data_arr):
-    # bot.forward_message(admin_chat_id, message.chat.id, message.message_id)
-    # bot.send_message(admin_chat_id, f"{' '.join(data_arr[1:])} {message.text}")
-    bot.send_message(admin_chat_id, f"#ФИО: {data_arr[1]} \nДата рождения: {data_arr[2]} \nПаспорт: {message.text}")
+def remember_user_phone_number(message, data_arr):
+    data_arr.append(message.text)
+    bot.send_message(message.chat.id, "Укажите город фактического проживания")
+    bot.register_next_step_handler(message, send_all_data_to_admin, data_arr)
+
+    logger.info("Пользователь %s указал свой номер телефона", get_user_full_name(message))
+
+
+def send_all_data_to_admin(message, data_arr):
+    bot.send_message(admin_chat_id, f"{data_arr[1]} \n{data_arr[2]} \n{data_arr[3]} \n{message.text}")
     text = f"Спасибо, что указали свои контактные данные, {data_arr[0]}! В ближайшее время с вами обязательно свяжутся!"
     bot.send_message(message.chat.id, text)
 
-    logger.info("Пользователь %s указал свои паспортные данные", get_user_full_name(message))
+    logger.info("Пользователь %s указал свой город", get_user_full_name(message))
 
 
 @bot.message_handler(commands=['info'])
