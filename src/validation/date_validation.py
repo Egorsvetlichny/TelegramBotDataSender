5e60a6b8652bf616356669614239503c2394f4d1
Egor Svetlichny <fkhor@inbox.ru>
2024-04-11 17:12:58 +0300

    add upgrade validation for user birthdate

--- src/validation/date_validation.py
+++ src/validation/date_validation.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta, date
 
 
 def validate_date(date_string):
@@ -9,10 +9,31 @@ def validate_date(date_string):
             return False
         if month < 1 or month > 12:
             return False
-        if year < datetime.date.today().year - 100 or year > 2006:
+        if year < date.today().year - 100 or year > 2006:
             return False
 
         return True
 
     except ValueError:
+        return False
+
+
+def validate_birthdate(birthdate_str):
+    try:
+        # Преобразование строки в объект datetime
+        birthdate = datetime.strptime(birthdate_str, '%Y-%m-%d')
+
+        # Проверка на возраст 18+
+        eighteen_years_ago = datetime.now() - timedelta(days=365 * 18)
+        if birthdate > eighteen_years_ago:
+            return False
+
+        # Проверка на возраст менее 100 лет
+        hundred_years_ago = datetime.now() - timedelta(days=365 * 100)
+        if birthdate < hundred_years_ago:
+            return False
+
+        return True
+    except ValueError:
+        # Если возникла ошибка при преобразовании строки в дату
         return False
